9818b32c10a0c587f464f6bd3835dc66852df922
Puripant <puripant@gmail.com>
2020-04-12 10:30:31 +0700

    Make charge parameter inversely proportional to the number of students/submissions; change coding style to ES6

--- mossViz.js
+++ mossViz.js
@@ -1,39 +1,37 @@
-var svg = d3.select("svg");
-var width = +svg.attr("width");
-var height = +svg.attr("height");
-var nodeRadius = 10;
+let svg = d3.select("svg");
+let width = +svg.attr("width");
+let height = +svg.attr("height");
+let nodeRadius = 10;
 
 // Get URL paramters
 const url = window.location.search;
-const queryStringRegEx = function(query) {
-	return new RegExp("[?|&]" + query + "=([^&]*)");
-};
-var matches = url.match(queryStringRegEx("id"));
+const queryStringRegEx = query => new RegExp("[?|&]" + query + "=([^&]*)");
+let matches = url.match(queryStringRegEx("id"));
 const id = matches ? matches[1] : undefined;
-var mossUrl = id? ("Moss Results" + id + ".html") : "Moss Results.html";
-// var mossUrl = id? ("http://moss.stanford.edu/results/" + id) : "Moss Results3.html";
-// var mossUrl = "Moss Results.html";
+let mossUrl = id? ("Moss Results" + id + ".html") : "Moss Results.html";
+// let mossUrl = id? ("http://moss.stanford.edu/results/" + id) : "Moss Results3.html";
+// let mossUrl = "Moss Results.html";
 
 const unwantedChars = ["/"];
 
-d3.html(mossUrl, function(error, data) {
+d3.html(mossUrl, (error, data) => {
 	if (error) throw error;
 
-	var rows = data.querySelector("tbody").querySelectorAll("tr");
+	let rows = data.querySelector("tbody").querySelectorAll("tr");
 
-	var nodeSet = new Set();
-	var pairs = [];
-	var maxWeight = -1;
-	var sumWeight = 0;
-	for(var r = 0; r < rows.length; r++) {
-		var columns = rows.item(r).querySelectorAll("td");
-		if(columns.length > 0) {
+	let nodeSet = new Set();
+	let pairs = [];
+	let maxWeight = -1;
+	let sumWeight = 0;
+	for (let r = 0; r < rows.length; r++) {
+		let columns = rows.item(r).querySelectorAll("td");
+		if (columns.length > 0) {
 			let pair = [];
-			for(var c = 0; c < 2; c++) { // two student ids
-				var text = columns.item(c).querySelector("a").textContent;
-				var nodeId = text.substring(0, text.lastIndexOf(" "));
+			for (let c = 0; c < 2; c++) { // two student ids
+				let text = columns.item(c).querySelector("a").textContent;
+				let nodeId = text.substring(0, text.lastIndexOf(" "));
 
-				unwantedChars.forEach(function(c) {
+				unwantedChars.forEach(c => {
 					nodeId = nodeId.replace(new RegExp(c, 'g'), "");
 				});
 
@@ -51,18 +49,18 @@ d3.html(mossUrl, function(error, data) {
 		}
 	}
 
-	var nodeArray = [...nodeSet];
-	var commonPrefixLen = longestCommonPrefixLength(nodeArray);
+	let nodeArray = [...nodeSet];
+	let commonPrefixLen = longestCommonPrefixLength(nodeArray);
 	nodes = [];
-	nodeArray = nodeArray.map(function(node) {
-		var shortNode = node.substring(commonPrefixLen);
+	nodeArray = nodeArray.map(node => {
+		let shortNode = node.substring(commonPrefixLen);
 		nodes.push({name: shortNode});
 		return shortNode;
-	});
+  });
 
-	var links = [];
-	for(let pair of pairs) {
-		if(pair[2] > sumWeight/pairs.length) {
+	let links = [];
+	for (let pair of pairs) {
+		if (pair[2] > sumWeight/pairs.length) {
 			links.push({
 				source: nodeArray.indexOf(pair[0].substring(commonPrefixLen)),
 				target: nodeArray.indexOf(pair[1].substring(commonPrefixLen)),
@@ -71,31 +69,32 @@ d3.html(mossUrl, function(error, data) {
 		}
 	}
 
-	var force = d3.layout.force()
+	let force = d3.layout.force()
 		// .gravity(.05)
-		.distance(function(d) { return (1-d.weight)*width/nodeRadius; })
-		.charge(-300)
+		.distance(d =>  (1-d.weight)*width/nodeRadius)
+		.charge(-10000/nodes.length) //NOTE should be inversely proportional to #students/submissions
 		.size([width, height])
 		.nodes(nodes)
 		.links(links)
 		.on("tick", ticked)
 		.start();
 
-	var link = svg.append("g")
+	let link = svg.append("g")
   		.attr("class", "links")
 		.selectAll("line")
 		.data(links)
 	    .enter().append("line")
-			.style("stroke-width", function(d) { return nodeRadius*d.weight; });
-			// .style("stroke-opacity", function(d) { return d.weight; });
+			.style("stroke-width", d => nodeRadius*d.weight);
+			// .style("stroke-opacity", d => d.weight);
 
-	var node = svg.append("g")
+	let node = svg.append("g")
   		.attr("class", "nodes")
   	.selectAll("circle")
 		.data(nodes)
 	    .enter().append("g")
 
-	node.append("circle")
+  node.append("circle")
+    // .style("display", d => (d.weight === 0)? "none" : "unset")
 		.attr("r", nodeRadius)
 		// .call(force.drag);
 		.call(d3.behavior.drag()
@@ -104,21 +103,18 @@ d3.html(mossUrl, function(error, data) {
 			.on("dragend", dragended));
 
 	node.append("text")
-		.attr("dy", "-1em")
-		.text(function(d) { return d.name; });
+    .attr("dy", "-1em")
+    // .style("display", d => (d.weight === 0)? "none" : "unset")
+		.text(d => d.name);
 
 	function ticked() {
-		node.attr("transform", function(d) {
-			return "translate(" + Math.max(2*nodeRadius, Math.min(width  - 2*nodeRadius, d.x)) + "," +
-														Math.max(2*nodeRadius, Math.min(height - 2*nodeRadius, d.y)) + ")";
-			// return "translate(" + d.x + "," + d.y + ")";
-		});
+		node.attr("transform", d => `translate(${Math.max(2*nodeRadius, Math.min(width  - 2*nodeRadius, d.x))},${Math.max(2*nodeRadius, Math.min(height - 2*nodeRadius, d.y))})`);
 
 		link
-	    .attr("x1", function(d) { return d.source.x; })
-	    .attr("y1", function(d) { return d.source.y; })
-	    .attr("x2", function(d) { return d.target.x; })
-	    .attr("y2", function(d) { return d.target.y; });
+	    .attr("x1", d => d.source.x)
+	    .attr("y1", d => d.source.y)
+	    .attr("x2", d => d.target.x)
+	    .attr("y2", d => d.target.y);
 	}
 
 	function dragstarted(d) {
@@ -142,20 +138,20 @@ d3.html(mossUrl, function(error, data) {
 	function longestCommonPrefixLength(texts) {
 		// // Automatic prefix truncation by finding common starting substring https://stackoverflow.com/questions/1916218/find-the-longest-common-starting-substring-in-a-set-of-strings/1917041#1917041
 		// nodeArray.sort();
-		// var i = 0;
+		// let i = 0;
 		// while(i < nodeArray[0].length && nodeArray[0].charAt(i) === nodeArray[nodes.length-1].charAt(i)) {
 		// 	i++;
 		// }
 
-    if(texts.length == 0) {
+    if (texts.length == 0) {
       return 0;
     }
 
-		var maxLen = texts[0].length;
-    for(var len = 0; len < maxLen; len++) {
-      var currentChar = texts[0].charAt(len);
-      for (var j = 1; j < texts.length; j++) {
-        if(len >= texts[j].length || texts[j].charAt(len) != currentChar) { //mismatch
+		let maxLen = texts[0].length;
+    for (let len = 0; len < maxLen; len++) {
+      let currentChar = texts[0].charAt(len);
+      for (let j = 1; j < texts.length; j++) {
+        if (len >= texts[j].length || texts[j].charAt(len) != currentChar) { //mismatch
           return len;
         }
       }
