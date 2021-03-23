92b5001694f0f7b6896be8d4d53fde537214356f
Puripant <puripant@gmail.com>
2021-03-24 00:47:00 +0700

    Update to d3v6 and remove some inessential features

--- mossViz.js
+++ mossViz.js
@@ -2,19 +2,11 @@ let svg = d3.select("svg");
 let width = +svg.attr("width");
 let height = +svg.attr("height");
 let nodeRadius = 10;
+let mossUrl = "Moss Results.html";
 
-// Get URL paramters
-const url = window.location.search;
-const queryStringRegEx = query => new RegExp("[?|&]" + query + "=([^&]*)");
-let matches = url.match(queryStringRegEx("id"));
-const id = matches ? matches[1] : undefined;
-let mossUrl = id? ("Moss Results" + id + ".html") : "Moss Results.html";
-// let mossUrl = id? ("http://moss.stanford.edu/results/" + id) : "Moss Results3.html";
-// let mossUrl = "Moss Results.html";
+let contain = (x, min, max) => Math.max(min, Math.min(max, x))
 
-const unwantedChars = ["/"];
-
-d3.html(mossUrl, (error, data) => {
+d3.html(mossUrl).then((data, error) => {
 	if (error) throw error;
 
 	let rows = data.querySelector("tbody").querySelectorAll("tr");
@@ -31,10 +23,6 @@ d3.html(mossUrl, (error, data) => {
 				let text = columns.item(c).querySelector("a").textContent;
 				let nodeId = text.substring(0, text.lastIndexOf(" "));
 
-				unwantedChars.forEach(c => {
-					nodeId = nodeId.replace(new RegExp(c, 'g'), "");
-				});
-
 				//TODO use percentage for link weight (but the percentages of two nodes are not necessary the same).
 
 				nodeSet.add(nodeId);
@@ -50,34 +38,49 @@ d3.html(mossUrl, (error, data) => {
 	}
 
 	let nodeArray = [...nodeSet];
-	let commonPrefixLen = longestCommonPrefixLength(nodeArray);
 	nodes = [];
 	nodeArray = nodeArray.map(node => {
-		let shortNode = node.substring(commonPrefixLen);
-		nodes.push({name: shortNode});
-		return shortNode;
+		nodes.push({name: node});
+		return node;
   });
 
 	let links = [];
 	for (let pair of pairs) {
 		if (pair[2] > sumWeight/pairs.length) {
 			links.push({
-				source: nodeArray.indexOf(pair[0].substring(commonPrefixLen)),
-				target: nodeArray.indexOf(pair[1].substring(commonPrefixLen)),
+				source: nodeArray.indexOf(pair[0]),
+				target: nodeArray.indexOf(pair[1]),
 				weight: pair[2]/maxWeight
 			});
 		}
 	}
 
-	let force = d3.layout.force()
-		// .gravity(.05)
-		.distance(d =>  (1-d.weight)*width/nodeRadius)
-		.charge(-10000/nodes.length) //NOTE should be inversely proportional to #students/submissions
-		.size([width, height])
-		.nodes(nodes)
-		.links(links)
-		.on("tick", ticked)
-		.start();
+	// let force = d3.layout.force()
+	// 	// .gravity(.05)
+	// 	.distance()
+	// 	.charge() 
+	// 	.size([width, height])
+	// 	.nodes(nodes)
+	// 	.links(links)
+	// 	.on("tick", ticked)
+	// 	.start();
+  let simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).distance(d =>  (1-d.weight)*width/nodeRadius))
+    .force("charge", d3.forceManyBody().strength(-1000/nodes.length)) //NOTE should be inversely proportional to #students/submissions
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", () => {
+      link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+
+      node.attr("transform", d => `translate(${contain(d.x, nodeRadius, width - nodeRadius)},` +
+                                  `${contain(d.y, nodeRadius, height - nodeRadius)})`);
+      // node
+      //     .attr("cx", d => d.x)
+      //     .attr("cy", d => d.y);
+    });
 
 	let link = svg.append("g")
   		.attr("class", "links")
@@ -97,65 +100,52 @@ d3.html(mossUrl, (error, data) => {
     // .style("display", d => (d.weight === 0)? "none" : "unset")
 		.attr("r", nodeRadius)
 		// .call(force.drag);
-		.call(d3.behavior.drag()
-			.on("dragstart", dragstarted)
-			.on("drag", dragged)
-			.on("dragend", dragended));
+		.call(d3.drag()
+      .on("start", event => {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      })
+      .on("drag", event => {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      })
+      .on("end", event => {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }));
 
 	node.append("text")
     .attr("dy", "-1em")
     // .style("display", d => (d.weight === 0)? "none" : "unset")
 		.text(d => d.name);
 
-	function ticked() {
-		node.attr("transform", d => `translate(${Math.max(2*nodeRadius, Math.min(width  - 2*nodeRadius, d.x))},${Math.max(2*nodeRadius, Math.min(height - 2*nodeRadius, d.y))})`);
+	// function ticked() {
+	// 	node.attr("transform", d => `translate(${Math.max(2*nodeRadius, Math.min(width  - 2*nodeRadius, d.x))},${Math.max(2*nodeRadius, Math.min(height - 2*nodeRadius, d.y))})`);
 
-		link
-	    .attr("x1", d => d.source.x)
-	    .attr("y1", d => d.source.y)
-	    .attr("x2", d => d.target.x)
-	    .attr("y2", d => d.target.y);
-	}
+	// 	link
+	//     .attr("x1", d => d.source.x)
+	//     .attr("y1", d => d.source.y)
+	//     .attr("x2", d => d.target.x)
+	//     .attr("y2", d => d.target.y);
+	// }
 
-	function dragstarted(d) {
-		force.stop()
-	}
+	// function dragstarted(d) {
+	// 	force.stop()
+	// }
 
-	function dragged(d) {
-		d.px += d3.event.dx;
-    d.py += d3.event.dy;
-    d.x += d3.event.dx;
-    d.y += d3.event.dy;
-    ticked();
-	}
+	// function dragged(d) {
+	// 	d.px += d3.event.dx;
+  //   d.py += d3.event.dy;
+  //   d.x += d3.event.dx;
+  //   d.y += d3.event.dy;
+  //   ticked();
+	// }
 
-	function dragended(d) {
-		d.fixed = true;
-		ticked();
-		force.resume();
-	}
-
-	function longestCommonPrefixLength(texts) {
-		// // Automatic prefix truncation by finding common starting substring https://stackoverflow.com/questions/1916218/find-the-longest-common-starting-substring-in-a-set-of-strings/1917041#1917041
-		// nodeArray.sort();
-		// let i = 0;
-		// while(i < nodeArray[0].length && nodeArray[0].charAt(i) === nodeArray[nodes.length-1].charAt(i)) {
-		// 	i++;
-		// }
-
-    if (texts.length == 0) {
-      return 0;
-    }
-
-		let maxLen = texts[0].length;
-    for (let len = 0; len < maxLen; len++) {
-      let currentChar = texts[0].charAt(len);
-      for (let j = 1; j < texts.length; j++) {
-        if (len >= texts[j].length || texts[j].charAt(len) != currentChar) { //mismatch
-          return len;
-        }
-      }
-    }
-    return maxLen;
-	}
-});
+	// function dragended(d) {
+	// 	d.fixed = true;
+	// 	ticked();
+	// 	force.resume();
+	// }
+})
