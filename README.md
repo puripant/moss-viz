e8f9318e24cee5e33a82b820295ab5fd335f38af
Puripant <puripant@gmail.com>
2021-03-24 01:13:33 +0700

    Clean up code

--- README.md
+++ README.md
@@ -5,9 +5,6 @@ Visualizing [Moss (Measure Of Software Similarity)](http://moss.stanford.edu) re
 Just open up `index.html` to see the visualization. It will open an example data file `Moss Results.html` which can be saved directly from the website.
 
 ## Visualization
-It is a standard node-link diagram implemented with [D3](https://d3js.org) (v3 for now). Each node is a submission path, truncated by the common prefix. An edge shows that two submissions are more similar than the average of the submission set (measured by the number of common lines and visualized as edge thickness).
+This standard node-link diagram is implemented with [D3](https://d3js.org). Each node is a submission shown with its filename. An edge links two submissions that are more similar than the average of the macthed lines of all submission pairs (visualized as edge thickness). Cliques of plagiarism should be easily detectable.
 
-Cliques of plagiarism should be easily detectable. Nodes can also be dragged around and fixed for manual layout. A simple bounded force layout is applied.
-
-## More to be Done
-It will be nice if the tool can directly fetch the result from an URL (without CORS errors). Advanced layout algorithms are welcome.
+A simple bounded force layout is applied. Nodes can also be dragged around and fixed for manual layout.
--- mossViz.js
+++ mossViz.js
@@ -19,20 +19,18 @@ d3.html(mossUrl).then((data, error) => {
 		let columns = rows.item(r).querySelectorAll("td");
 		if (columns.length > 0) {
 			let pair = [];
-			for (let c = 0; c < 2; c++) { // two student ids
+			for (let c = 0; c < 2; c++) { // two assignment submissions
 				let text = columns.item(c).querySelector("a").textContent;
 				let nodeId = text.substring(0, text.lastIndexOf(" "));
-
-				//TODO use percentage for link weight (but the percentages of two nodes are not necessary the same).
 
 				nodeSet.add(nodeId);
 				pair.push(nodeId);
 			}
 
-			let weight = +columns.item(2).textContent
+			let weight = +columns.item(2).textContent; // matched lines
 			maxWeight = Math.max(maxWeight, weight);
 			sumWeight += weight;
-			pair.push(weight); // #common lines
+			pair.push(weight);
 			pairs.push(pair);
 		}
 	}
@@ -55,18 +53,9 @@ d3.html(mossUrl).then((data, error) => {
 		}
 	}
 
-	// let force = d3.layout.force()
-	// 	// .gravity(.05)
-	// 	.distance()
-	// 	.charge() 
-	// 	.size([width, height])
-	// 	.nodes(nodes)
-	// 	.links(links)
-	// 	.on("tick", ticked)
-	// 	.start();
   let simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).distance(d =>  (1-d.weight)*width/nodeRadius))
-    .force("charge", d3.forceManyBody().strength(-1000/nodes.length)) //NOTE should be inversely proportional to #students/submissions
+    .force("link", d3.forceLink(links).distance(d => (1.5-d.weight)*width/nodeRadius))
+    .force("charge", d3.forceManyBody().strength(-1000/nodes.length))
     .force("center", d3.forceCenter(width / 2, height / 2))
     .on("tick", () => {
       link
@@ -75,11 +64,8 @@ d3.html(mossUrl).then((data, error) => {
           .attr("x2", d => d.target.x)
           .attr("y2", d => d.target.y);
 
-      node.attr("transform", d => `translate(${contain(d.x, nodeRadius, width - nodeRadius)},` +
-                                  `${contain(d.y, nodeRadius, height - nodeRadius)})`);
-      // node
-      //     .attr("cx", d => d.x)
-      //     .attr("cy", d => d.y);
+      node.attr("transform", d => `translate(${contain(d.x, nodeRadius, width  - nodeRadius)},` +
+                                            `${contain(d.y, nodeRadius, height - nodeRadius)})`);
     });
 
 	let link = svg.append("g")
@@ -88,7 +74,6 @@ d3.html(mossUrl).then((data, error) => {
 		.data(links)
 	    .enter().append("line")
 			.style("stroke-width", d => nodeRadius*d.weight);
-			// .style("stroke-opacity", d => d.weight);
 
 	let node = svg.append("g")
   		.attr("class", "nodes")
@@ -97,55 +82,17 @@ d3.html(mossUrl).then((data, error) => {
 	    .enter().append("g")
 
   node.append("circle")
-    // .style("display", d => (d.weight === 0)? "none" : "unset")
 		.attr("r", nodeRadius)
-		// .call(force.drag);
 		.call(d3.drag()
-      .on("start", event => {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-      })
       .on("drag", event => {
         event.subject.fx = event.x;
         event.subject.fy = event.y;
       })
       .on("end", event => {
-        if (!event.active) simulation.alphaTarget(0);
-        event.subject.fx = null;
-        event.subject.fy = null;
+        if (!event.active) simulation.alphaTarget(0.3).restart();
       }));
 
 	node.append("text")
     .attr("dy", "-1em")
-    // .style("display", d => (d.weight === 0)? "none" : "unset")
 		.text(d => d.name);
-
-	// function ticked() {
-	// 	node.attr("transform", d => `translate(${Math.max(2*nodeRadius, Math.min(width  - 2*nodeRadius, d.x))},${Math.max(2*nodeRadius, Math.min(height - 2*nodeRadius, d.y))})`);
-
-	// 	link
-	//     .attr("x1", d => d.source.x)
-	//     .attr("y1", d => d.source.y)
-	//     .attr("x2", d => d.target.x)
-	//     .attr("y2", d => d.target.y);
-	// }
-
-	// function dragstarted(d) {
-	// 	force.stop()
-	// }
-
-	// function dragged(d) {
-	// 	d.px += d3.event.dx;
-  //   d.py += d3.event.dy;
-  //   d.x += d3.event.dx;
-  //   d.y += d3.event.dy;
-  //   ticked();
-	// }
-
-	// function dragended(d) {
-	// 	d.fixed = true;
-	// 	ticked();
-	// 	force.resume();
-	// }
 })
